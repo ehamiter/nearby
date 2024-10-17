ed5a50d3719dcd25c89e2f3adb6a8251361fe528
Eric Hamiter <ehamiter@gmail.com>
2024-10-17 17:39:57 -0500

    Initial Commit

---
+++ nearby/nearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  nearbyApp.swift
+//  nearby
+//
+//  Created by Eric on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct nearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nearbyTests/nearbyTests.swift
@@ -0,0 +1,17 @@
+//
+//  nearbyTests.swift
+//  nearbyTests
+//
+//  Created by Eric on 10/17/24.
+//
+
+import Testing
+@testable import nearby
+
+struct nearbyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
